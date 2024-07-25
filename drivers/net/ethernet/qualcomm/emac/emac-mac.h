b4adac6af4dc962f79ed8d83124aac6cc198d6ec
lakshmi pokuri <quic_lpokuri@quicinc.com>
2024-07-25 21:11:11 +0530

    net: emac: Bring-up the eth0 i/f on 9x07
    included mac/phy changes.
    
    Change-Id: I5029c6670b9a71d78069e8cdacb16a1520ff79c9
    Signed-off-by: lakshmi pokuri <quic_lpokuri@quicinc.com>

--- drivers/net/ethernet/qualcomm/emac/emac-mac.h
+++ drivers/net/ethernet/qualcomm/emac/emac-mac.h
@@ -15,226 +15,134 @@
 #ifndef _EMAC_HW_H_
 #define _EMAC_HW_H_
 
-/* EMAC_CSR register offsets */
-#define EMAC_EMAC_WRAPPER_CSR1                                0x000000
-#define EMAC_EMAC_WRAPPER_CSR2                                0x000004
-#define EMAC_EMAC_WRAPPER_TX_TS_LO                            0x000104
-#define EMAC_EMAC_WRAPPER_TX_TS_HI                            0x000108
-#define EMAC_EMAC_WRAPPER_TX_TS_INX                           0x00010c
+#include <linux/mii.h>
 
-/* DMA Order Settings */
-enum emac_dma_order {
-	emac_dma_ord_in = 1,
-	emac_dma_ord_enh = 2,
-	emac_dma_ord_out = 4
-};
+#include "emac.h"
+#include "emac-regs.h"
+#include "emac-defines.h"
 
-enum emac_dma_req_block {
-	emac_dma_req_128 = 0,
-	emac_dma_req_256 = 1,
-	emac_dma_req_512 = 2,
-	emac_dma_req_1024 = 3,
-	emac_dma_req_2048 = 4,
-	emac_dma_req_4096 = 5
-};
+/* function prototype */
 
-/* Returns the value of bits idx...idx+n_bits */
-#define BITS_GET(val, lo, hi) ((le32_to_cpu(val) & GENMASK((hi), (lo))) >> lo)
-#define BITS_SET(val, lo, hi, new_val) \
-	val = cpu_to_le32((le32_to_cpu(val) & (~GENMASK((hi), (lo)))) |	\
-		(((new_val) << (lo)) & GENMASK((hi), (lo))))
+/* REG */
+u32 emac_reg_r32(struct emac_hw *hw, u8 base, u32 reg);
+void emac_reg_w32(struct emac_hw *hw, u8 base, u32 reg, u32 val);
+void emac_reg_update32(struct emac_hw *hw, u8 base, u32 reg,
+		       u32 mask, u32 val);
+u32 emac_reg_field_r32(struct emac_hw *hw, u8 base, u32 reg,
+		       u32 mask, u32 shift);
+void emac_hw_config_pow_save(struct emac_hw *hw, u32 speed, bool wol_en,
+			     bool rx_en);
+/* MAC */
+void emac_hw_enable_intr(struct emac_hw *hw);
+void emac_hw_disable_intr(struct emac_hw *hw);
+void emac_hw_set_mc_addr(struct emac_hw *hw, u8 *addr);
+void emac_hw_clear_mc_addr(struct emac_hw *hw);
 
-/* RRD (Receive Return Descriptor) */
-struct emac_rrd {
-	u32	word[6];
+void emac_hw_config_mac_ctrl(struct emac_hw *hw);
+void emac_hw_config_rss(struct emac_hw *hw);
+void emac_hw_config_wol(struct emac_hw *hw, u32 wufc);
+int emac_hw_config_fc(struct emac_hw *hw);
 
-/* number of RFD */
-#define RRD_NOR(rrd)			BITS_GET((rrd)->word[0], 16, 19)
-/* start consumer index of rfd-ring */
-#define RRD_SI(rrd)			BITS_GET((rrd)->word[0], 20, 31)
-/* vlan-tag (CVID, CFI and PRI) */
-#define RRD_CVALN_TAG(rrd)		BITS_GET((rrd)->word[2], 0, 15)
-/* length of the packet */
-#define RRD_PKT_SIZE(rrd)		BITS_GET((rrd)->word[3], 0, 13)
-/* L4(TCP/UDP) checksum failed */
-#define RRD_L4F(rrd)			BITS_GET((rrd)->word[3], 14, 14)
-/* vlan tagged */
-#define RRD_CVTAG(rrd)			BITS_GET((rrd)->word[3], 16, 16)
-/* When set, indicates that the descriptor is updated by the IP core.
- * When cleared, indicates that the descriptor is invalid.
- */
-#define RRD_UPDT(rrd)			BITS_GET((rrd)->word[3], 31, 31)
-#define RRD_UPDT_SET(rrd, val)		BITS_SET((rrd)->word[3], 31, 31, val)
-/* timestamp low */
-#define RRD_TS_LOW(rrd)			BITS_GET((rrd)->word[4], 0, 29)
-/* timestamp high */
-#define RRD_TS_HI(rrd)			le32_to_cpu((rrd)->word[5])
-};
+void emac_hw_reset_mac(struct emac_hw *hw);
+void emac_hw_config_mac(struct emac_hw *hw);
+void emac_hw_start_mac(struct emac_hw *hw);
+void emac_hw_stop_mac(struct emac_hw *hw);
 
-/* TPD (Transmit Packet Descriptor) */
-struct emac_tpd {
-	u32				word[4];
+void emac_hw_set_mac_addr(struct emac_hw *hw, u8 *addr);
 
-/* Number of bytes of the transmit packet. (include 4-byte CRC) */
-#define TPD_BUF_LEN_SET(tpd, val)	BITS_SET((tpd)->word[0], 0, 15, val)
-/* Custom Checksum Offload: When set, ask IP core to offload custom checksum */
-#define TPD_CSX_SET(tpd, val)		BITS_SET((tpd)->word[1], 8, 8, val)
-/* TCP Large Send Offload: When set, ask IP core to do offload TCP Large Send */
-#define TPD_LSO(tpd)			BITS_GET((tpd)->word[1], 12, 12)
-#define TPD_LSO_SET(tpd, val)		BITS_SET((tpd)->word[1], 12, 12, val)
-/*  Large Send Offload Version: When set, indicates this is an LSOv2
- * (for both IPv4 and IPv6). When cleared, indicates this is an LSOv1
- * (only for IPv4).
- */
-#define TPD_LSOV_SET(tpd, val)		BITS_SET((tpd)->word[1], 13, 13, val)
-/* IPv4 packet: When set, indicates this is an  IPv4 packet, this bit is only
- * for LSOV2 format.
- */
-#define TPD_IPV4_SET(tpd, val)		BITS_SET((tpd)->word[1], 16, 16, val)
-/* 0: Ethernet   frame (DA+SA+TYPE+DATA+CRC)
- * 1: IEEE 802.3 frame (DA+SA+LEN+DSAP+SSAP+CTL+ORG+TYPE+DATA+CRC)
- */
-#define TPD_TYP_SET(tpd, val)		BITS_SET((tpd)->word[1], 17, 17, val)
-/* Low-32bit Buffer Address */
-#define TPD_BUFFER_ADDR_L_SET(tpd, val)	((tpd)->word[2] = cpu_to_le32(val))
-/* CVLAN Tag to be inserted if INS_VLAN_TAG is set, CVLAN TPID based on global
- * register configuration.
- */
-#define TPD_CVLAN_TAG_SET(tpd, val)	BITS_SET((tpd)->word[3], 0, 15, val)
-/*  Insert CVlan Tag: When set, ask MAC to insert CVLAN TAG to outgoing packet
- */
-#define TPD_INSTC_SET(tpd, val)		BITS_SET((tpd)->word[3], 17, 17, val)
-/* High-14bit Buffer Address, So, the 64b-bit address is
- * {DESC_CTRL_11_TX_DATA_HIADDR[17:0],(register) BUFFER_ADDR_H, BUFFER_ADDR_L}
- * Extend TPD_BUFFER_ADDR_H to [31, 18], because we never enable timestamping.
- */
-#define TPD_BUFFER_ADDR_H_SET(tpd, val)	BITS_SET((tpd)->word[3], 18, 31, val)
-/* Format D. Word offset from the 1st byte of this packet to start to calculate
- * the custom checksum.
- */
-#define TPD_PAYLOAD_OFFSET_SET(tpd, val) BITS_SET((tpd)->word[1], 0, 7, val)
-/*  Format D. Word offset from the 1st byte of this packet to fill the custom
- * checksum to
- */
-#define TPD_CXSUM_OFFSET_SET(tpd, val)	BITS_SET((tpd)->word[1], 18, 25, val)
+/* TX Timestamp */
+bool emac_hw_read_tx_tstamp(struct emac_hw *hw, struct emac_hwtxtstamp *ts);
 
-/* Format C. TCP Header offset from the 1st byte of this packet. (byte unit) */
-#define TPD_TCPHDR_OFFSET_SET(tpd, val)	BITS_SET((tpd)->word[1], 0, 7, val)
-/* Format C. MSS (Maximum Segment Size) got from the protocol layer. (byte unit)
- */
-#define TPD_MSS_SET(tpd, val)		BITS_SET((tpd)->word[1], 18, 30, val)
-/* packet length in ext tpd */
-#define TPD_PKT_LEN_SET(tpd, val)	((tpd)->word[2] = cpu_to_le32(val))
-};
+#define IMR_NORMAL_MASK		(ISR_ERROR | ISR_OVER | ISR_TX_PKT)
 
-/* emac_ring_header represents a single, contiguous block of DMA space
- * mapped for the three descriptor rings (tpd, rfd, rrd)
- */
-struct emac_ring_header {
-	void			*v_addr;	/* virtual address */
-	dma_addr_t		dma_addr;	/* dma address */
-	size_t			size;		/* length in bytes */
-	size_t			used;
-};
+#define IMR_EXTENDED_MASK       (\
+		SW_MAN_INT      |\
+		ISR_OVER        |\
+		ISR_ERROR       |\
+		ISR_TX_PKT)
 
-/* emac_buffer is wrapper around a pointer to a socket buffer
- * so a DMA handle can be stored along with the skb
- */
-struct emac_buffer {
-	struct sk_buff		*skb;		/* socket buffer */
-	u16			length;		/* rx buffer length */
-	dma_addr_t		dma_addr;	/* dma address */
-};
+#define ISR_RX_PKT      (\
+	RX_PKT_INT0     |\
+	RX_PKT_INT1     |\
+	RX_PKT_INT2     |\
+	RX_PKT_INT3)
 
-/* receive free descriptor (rfd) ring */
-struct emac_rfd_ring {
-	struct emac_buffer	*rfbuff;
-	u32			*v_addr;	/* virtual address */
-	dma_addr_t		dma_addr;	/* dma address */
-	size_t			size;		/* length in bytes */
-	unsigned int		count;		/* number of desc in the ring */
-	unsigned int		produce_idx;
-	unsigned int		process_idx;
-	unsigned int		consume_idx;	/* unused */
-};
+#define ISR_TX_PKT      (\
+	TX_PKT_INT      |\
+	TX_PKT_INT1     |\
+	TX_PKT_INT2     |\
+	TX_PKT_INT3)
 
-/* Receive Return Desciptor (RRD) ring */
-struct emac_rrd_ring {
-	u32			*v_addr;	/* virtual address */
-	dma_addr_t		dma_addr;	/* physical address */
-	size_t			size;		/* length in bytes */
-	unsigned int		count;		/* number of desc in the ring */
-	unsigned int		produce_idx;	/* unused */
-	unsigned int		consume_idx;
-};
+#define ISR_GPHY_LINK        (\
+	GPHY_LINK_UP_INT     |\
+	GPHY_LINK_DOWN_INT)
 
-/* Rx queue */
-struct emac_rx_queue {
-	struct net_device	*netdev;	/* netdev ring belongs to */
-	struct emac_rrd_ring	rrd;
-	struct emac_rfd_ring	rfd;
-	struct napi_struct	napi;
-	struct emac_irq		*irq;
+#define ISR_OVER        (\
+	RFD0_UR_INT     |\
+	RFD1_UR_INT     |\
+	RFD2_UR_INT     |\
+	RFD3_UR_INT     |\
+	RFD4_UR_INT     |\
+	RXF_OF_INT      |\
+	TXF_UR_INT)
 
-	u32			intr;
-	u32			produce_mask;
-	u32			process_mask;
-	u32			consume_mask;
+#define ISR_ERROR       (\
+	DMAR_TO_INT     |\
+	DMAW_TO_INT     |\
+	TXQ_TO_INT)
 
-	u16			produce_reg;
-	u16			process_reg;
-	u16			consume_reg;
+#define REG_MAC_RX_STATUS_BIN           EMAC_RXMAC_STATC_REG0
+#define REG_MAC_RX_STATUS_END           EMAC_RXMAC_STATC_REG22
+#define REG_MAC_TX_STATUS_BIN           EMAC_TXMAC_STATC_REG0
+#define REG_MAC_TX_STATUS_END           EMAC_TXMAC_STATC_REG24
 
-	u8			produce_shift;
-	u8			process_shft;
-	u8			consume_shift;
-};
+#define RXQ0_NUM_RFD_PREF_DEF           8
+#define TXQ0_NUM_TPD_PREF_DEF           5
 
-/* Transimit Packet Descriptor (tpd) ring */
-struct emac_tpd_ring {
-	struct emac_buffer	*tpbuff;
-	u32			*v_addr;	/* virtual address */
-	dma_addr_t		dma_addr;	/* dma address */
+#define EMAC_PREAMBLE_DEF               7
 
-	size_t			size;		/* length in bytes */
-	unsigned int		count;		/* number of desc in the ring */
-	unsigned int		produce_idx;
-	unsigned int		consume_idx;
-	unsigned int		last_produce_idx;
-};
+#define DMAR_DLY_CNT_DEF                15
+#define DMAW_DLY_CNT_DEF                4
 
-/* Tx queue */
-struct emac_tx_queue {
-	struct emac_tpd_ring	tpd;
+#define MDIO_CLK_25_4                   0
 
-	u32			produce_mask;
-	u32			consume_mask;
+#define RXQ0_RSS_HSTYP_IPV6_TCP_EN      0x20
+#define RXQ0_RSS_HSTYP_IPV6_EN          0x10
+#define RXQ0_RSS_HSTYP_IPV4_TCP_EN      0x8
+#define RXQ0_RSS_HSTYP_IPV4_EN          0x4
 
-	u16			max_packets;	/* max packets per interrupt */
-	u16			produce_reg;
-	u16			consume_reg;
+#define MASTER_CTRL_CLK_SEL_DIS         0x1000
 
-	u8			produce_shift;
-	u8			consume_shift;
-};
+#define MDIO_WAIT_TIMES                 1000
 
-struct emac_adapter;
+/* PHY */
+#define MII_PSSR                        0x11 /* PHY Specific Status Reg */
+#define MII_DBG_ADDR                    0x1D /* PHY Debug Address Reg */
+#define MII_DBG_DATA                    0x1E /* PHY Debug Data Reg */
+#define MII_INT_ENABLE			0x12 /* PHY Interrupt Enable Reg */
+#define MII_INT_STATUS			0x13 /* PHY Interrupt Status Reg */
 
-int  emac_mac_up(struct emac_adapter *adpt);
-void emac_mac_down(struct emac_adapter *adpt);
-void emac_mac_reset(struct emac_adapter *adpt);
-void emac_mac_stop(struct emac_adapter *adpt);
-void emac_mac_mode_config(struct emac_adapter *adpt);
-void emac_mac_rx_process(struct emac_adapter *adpt, struct emac_rx_queue *rx_q,
-			 int *num_pkts, int max_pkts);
-int emac_mac_tx_buf_send(struct emac_adapter *adpt, struct emac_tx_queue *tx_q,
-			 struct sk_buff *skb);
-void emac_mac_tx_process(struct emac_adapter *adpt, struct emac_tx_queue *tx_q);
-void emac_mac_rx_tx_ring_init_all(struct platform_device *pdev,
-				  struct emac_adapter *adpt);
-int  emac_mac_rx_tx_rings_alloc_all(struct emac_adapter *adpt);
-void emac_mac_rx_tx_rings_free_all(struct emac_adapter *adpt);
-void emac_mac_multicast_addr_clear(struct emac_adapter *adpt);
-void emac_mac_multicast_addr_set(struct emac_adapter *adpt, u8 *addr);
+/* MII_BMCR (0x00) */
+#define BMCR_SPEED10                    0x0000
 
+/* MII_PSSR (0x11) */
+#define PSSR_FC_RXEN                    0x0004
+#define PSSR_FC_TXEN                    0x0008
+#define PSSR_SPD_DPLX_RESOLVED          0x0800  /* 1=Speed & Duplex resolved */
+#define PSSR_DPLX                       0x2000  /* 1=Duplex 0=Half Duplex */
+#define PSSR_SPEED                      0xC000  /* Speed, bits 14:15 */
+#define PSSR_10MBS                      0x0000  /* 00=10Mbs */
+#define PSSR_100MBS                     0x4000  /* 01=100Mbs */
+#define PSSR_1000MBS                    0x8000  /* 10=1000Mbs */
+
+/* MII DBG registers */
+#define HIBERNATE_CTRL_REG              0xB
+
+/* HIBERNATE_CTRL_REG */
+#define HIBERNATE_EN                    0x8000
+
+/* MII_INT_ENABLE/MII_INT_STATUS */
+#define LINK_SUCCESS_INTERRUPT			BIT(10)
+#define LINK_SUCCESS_BX			BIT(7)
+#define WOL_INT				BIT(0)
 #endif /*_EMAC_HW_H_*/
