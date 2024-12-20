08ce0e77bdc6e3d87d2e36f7d04d596f305b03f1
Akash Kumar <quic_akakum@quicinc.com>
2024-12-20 14:05:05 +0530

    usb: dwc3: Reserve Higher Bandwidth for HS Periodic EPs
    It is observed while testing multiple audio
    devices, a glitch is observed during testing.
    As per dwc datasheet,By default, HC reserves 80% of the bandwidth
    for periodic EPs.
    
    Add quirk to set GUCTL register BIT 16 to accommodate higher
    bandwidth for 2 isoc eps.
    
    If this bit is set, the bandwidth is relaxed to 85% to
    accommodate two high speed, high bandwidth ISOC EPs.
    USB 2.0 required 80% bandwidth allocated for ISOC traffic. If
    two High-bandwidth ISOC devices (HD Webcams) are
    connected, and if each requires 1024-bytes X 3 packets per
    Micro-Frame, then the bandwidth required is around 82%. If
    this bit is set, then it is possible to connect two Webcams of
    1024bytes X 3 paylod per Micro-Frame each. Alternatively, you
    might need to lower the resolution of the webcams.
    This bit is valid in Host and DRD configuration and is used in
    host mode operation only.
    Set this bit for host mode uvc uac usecases where two isoc eps
    are used and flicker is seen.
    
    Change-Id: Ia4a8df486be2ff21d39d7824352a17343f08f057
    Signed-off-by: Akash Kumar <quic_akakum@quicinc.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1090,6 +1090,14 @@ int dwc3_core_init(struct dwc3 *dwc)
 		dwc3_writel(dwc->regs, DWC3_GUCTL1, reg);
 	}
 
+	if (dwc->revision >= DWC3_REVISION_250A) {
+		if (dwc->dwc3_guctl_resbwhseps_quirk) {
+			reg = dwc3_readl(dwc->regs, DWC3_GUCTL);
+			reg |= DWC3_GUCTL_RESBWHSEPS;
+			dwc3_writel(dwc->regs, DWC3_GUCTL, reg);
+		}
+	}
+
 	/*
 	 * Must config both number of packets and max burst settings to enable
 	 * RX and/or TX threshold.
@@ -1475,6 +1483,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				"snps,dis-tx-ipgap-linecheck-quirk");
 	dwc->parkmode_disable_ss_quirk = device_property_read_bool(dev,
 				"snps,parkmode-disable-ss-quirk");
+	dwc->dwc3_guctl_resbwhseps_quirk = device_property_read_bool(dev,
+				"snps,dwc3_guctl_resbwhseps_quirk");
 
 	dwc->tx_de_emphasis_quirk = device_property_read_bool(dev,
 				"snps,tx_de_emphasis_quirk");
