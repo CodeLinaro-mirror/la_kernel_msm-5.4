arter97/caf_msm-5.15

drivers: iio: imu: Disable IAM20680 sensor once buffer completed
1) Disable the IAM20680 sensor once buffering of sensor data on boot up
completed, disable only if there is no active client to sensor.

2) Removed the msleep as sensor probe was taking more time when iam20680
is not present in target.

Change-Id: Id9961015c97d685e29cb3b05a24cb16d238887cc
Signed-off-by: Puneet Yatnal <quic_puneety@quicinc.com>
Signed-off-by: Shivali M S <quic_shivms@quicinc.com>

drivers/iio/imu/inv_mpu/iam20680/inv_mpu_core_20680.c
@@ -413,6 +413,12 @@ static ssize_t inv_sensor_on_store(struct device *dev,
 	pr_debug("sensor [%s] requested  %s, input [%d]\n",
 			sensor_l_info[ind], (on == 1) ? "On" : "Off", data);
 
+#ifdef CONFIG_ENABLE_ASM_ACC_GYRO_BUFFERING
+	if (ind == SENSOR_L_ACCEL)
+		st->accel_enable = on;
+	if (ind == SENSOR_L_GYRO)
+		st->gyro_enable = on;
+#endif
 	if (on == st->sensor_l[ind].on) {
 		pr_debug("sensor [%s] is already %s, input [%d]\n",
 			sensor_l_info[ind], (on == 1) ? "On" : "Off", data);

CodeLinaro-mirror/la_kernel_msm-5.4

Signed-off-by: Saurav Shah <quic_sauravs@quicinc.com>

drivers/iio/imu/inv_mpu/iam20680/inv_mpu_core_20680.c
@@ -441,6 +441,12 @@ static ssize_t inv_sensor_on_store(struct device *dev,
 	pr_debug("sensor [%s] requested  %s, input [%d]\n",
 			sensor_l_info[ind], (on == 1) ? "On" : "Off", data);
 
+#ifdef CONFIG_ENABLE_IAM_ACC_GYRO_BUFFERING
+	if (ind == SENSOR_L_ACCEL)
+		st->accel_enable = on;
+	if (ind == SENSOR_L_GYRO)
+		st->gyro_enable = on;
+#endif
 	if (on == st->sensor_l[ind].on) {
 		pr_debug("sensor [%s] is already %s, input [%d]\n",
 			sensor_l_info[ind], (on == 1) ? "On" : "Off", data);